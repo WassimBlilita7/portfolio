5f8e68684f6e591d7ecf720046bc7deb5f23cec2
wassimblilita7 <wassim.blilita19@gmail.com>
2025-02-05 13:41:45 +0100

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="wassim.blilita19@gmail.com">wassim.blilita19@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/mohamed-wassim-blilita-27a01a216/">linkedin.com/BlilitaMoahedWassim</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/wassimblilita7">github.com/wassimblilita7</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
